4908fdd2b46894a07b9d3ca06d79e6abd005182e
Juan Sebastián Núñez Guzmán <juan.nunezg@upb.edu.co>
2026-07-16 11:18:11 -0500

    hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,12 @@
+# mi_primer_script.py
+
+print("¡hola, Github!")
+
+numeros = list(range(1, 1001))
+
+print("Números del 1 al 1000:")
+for n in numeros:
+    print(n)
+
+suma_total = sum(numeros)
+print("\nSuma de los números del 1 al 1000:", suma_total)
